c790a8e0ca12cfb1fd42fe0d44630cb44d7f5021
xiaocai771211682 <94445606+xiaocai771211682@users.noreply.github.com>
2025-06-25 22:28:59 +0800

    Update stock_web.py

--- stock_web.py
+++ stock_web.py
@@ -21,7 +21,7 @@ logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(levelname)s - %(message)s',
     handlers=[
-        logging.FileHandler('app.log'),
+        logging.FileHandler('/tmp/app.log'),
         logging.StreamHandler()
     ]
 )
